3c401d8ec498715390213d681ef2ffe8cae3e3ce
Junyo <junyogabriel001@hotmail.com>
2020-01-15 16:16:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ bot.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 
-for(const file of commandsFiles){
+for(const file of commandFiles){
 	const command = require(`./commands/${file}`);
 
 	bot.commands.set(command.nome, command);
