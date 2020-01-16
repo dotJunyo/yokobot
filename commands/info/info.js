e4a55b64f64b29b1a573ff0933ca8e33dc72bd08
Junyo <junyogabriel001@hotmail.com>
2020-01-16 08:38:24 -0300

    Update info.js

--- commands/info/info.js
+++ commands/info/info.js
@@ -42,7 +42,7 @@ module.exports = {
         .setTimestamp()
 
         if(member.user.presence.game)
-            infoEmbed.addField(`**> Jogando:** ${member.user.presence.game.name}`)
+            infoEmbed.addField(stripIndents`**> Jogando:** ${member.user.presence.game.name}`)
 
         message.channel.send(infoEmbed);
 
