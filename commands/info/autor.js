79d153d44a353730fb0081680574c50654a87e33
Junyo <junyogabriel001@hotmail.com>
2020-01-16 03:46:25 -0300

    Update autor.js

--- commands/info/autor.js
+++ commands/info/autor.js
@@ -4,7 +4,7 @@ module.exports = {
     description: "A Yoko fala quem é seu criador!",
     run: async(bot, message, args) => {
 
-        const autorID = bot.fetchUser(295718290251317260).avatarURL();
+        /*const autorID = bot.fetchUser(295718290251317260).avatarURL();
         const autorName = bot.fetchUser(295718290251317260).username();
 
         const autorEmbed = new Discord.RichEmbed()
@@ -12,7 +12,7 @@ module.exports = {
         .setTitle(`Foto de ${autorName}`)
         .setImage(autorID);
         message.channel.send(autorEmbed);
-        
+*/
         message.channel.send('Fui criada por dotJunyo! (Ele é um gato)');
     },
 }
