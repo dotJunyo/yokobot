ff5072e4328f35c3cbb81e506d4afd075ed7878a
Junyo <junyogabriel001@hotmail.com>
2020-01-16 06:14:10 -0300

    Update avatar.js

--- commands/variados/avatar.js
+++ commands/variados/avatar.js
@@ -1,11 +1,17 @@
+const { getMember, formatDate } = require("../../functions.js");
+const { RichEmbed } =require("discord.js");
+const { stripIndents } = require("common-tags");
+
 module.exports = {
     name: "avatar",
     category: "variados",
     description: "Mostra o avatar de algum usuário",
     run: async(bot, message, args) => {
 
+        const member = getMember(message, args.join(" "));
+
         if(!args[1]){                 
-            const avatarEmbed = new Discord.RichEmbed()
+            const avatarEmbed = new RichEmbed()
             .setColor('#275BF0')
             .setTitle(message.author.username +", que foto linda ^^")
             .setImage(message.author.avatarURL);
@@ -14,7 +20,7 @@ module.exports = {
 
             var mentioned = message.mentions.users.first();
 
-            const avatarEmbed = new Discord.RichEmbed()
+            const avatarEmbed = new RichEmbed()
             .setColor('#275BF0')
             .setTitle(`Foto de ${mentioned.username}`)
             .setImage(mentioned.avatarURL);
