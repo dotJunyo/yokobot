b2de1e044eec93329f145979bf2d7659c6635fa3
Junyo <junyogabriel001@hotmail.com>
2020-01-16 05:39:07 -0300

    Update info.js

--- commands/info/info.js
+++ commands/info/info.js
@@ -28,19 +28,19 @@ module.exports = {
         .setThumbnail(member.user.displayAvatarURl)
         .setColor("#00ffff")
 
-        .addField('Informação do membro', stripIndents`**>Display Name:** ${member.displayName}
+        .addField('Informação do membro', stripIndents`**> Display Name:** ${member.displayName}
             **> Entrou em:** ${joined}
             **> Cargos:** ${roles}`, true)
 
         .addField('Informação de usuário', stripIndents`**> ID:** ${member.user.id}
-            **>Username:** ${member.user.username}
-            **>Discord Tag:** ${member.user.tag}
-            **>Criou em:** ${created}`, true)
+            **> Username:** ${member.user.username}
+            **> Discord Tag:** ${member.user.tag}
+            **> Criou em:** ${created}`, true)
 
         .setTimestamp()
 
         if(member.user.presence.game)
-            infoEmbed.addField('Jogando atualmente', `**>Nome:** ${member.user.presence.game.name}`)
+            infoEmbed.addField('Jogando atualmente', `**> Nome:** ${member.user.presence.game.name}`)
 
         message.channel.send(infoEmbed);
 
