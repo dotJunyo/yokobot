4b912c421bbcf362847fb5bbf78d6ade9af54828
Junyo <junyogabriel001@hotmail.com>
2020-01-16 08:06:47 -0300

    Update uptime.js

--- commands/info/uptime.js
+++ commands/info/uptime.js
@@ -17,34 +17,34 @@ module.exports = {
 
         let tempoOnline = `Eu tô acordada a ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos ^-^`;
 
-        let tempoOn = "Tô meio dormindo ainda k";
+        var tempoOn = "Tô meio dormindo ainda k";
 
 
 
-if(minutos <= 10){
-    tempoOn = "Acordei agorinha =P";
-}else if(horas <= 12){
-    tempoOn = "Tá na hora de ir dormir já k"
-}else if(dias <= 1){
-    tempoOn = "Alguém me deixa dormir pfv ;-;"
-}else if(dias <= 7){
-    tempoOn = "Daqui a pouco já vai fazer uma semana, é sério, deixa eu dormir..."
-}else{
-    tempoOn = "Virei sócia da RedBull, não preciso dormir mais \o/"
-}
+        if(minutos <= 10){
+            tempoOn = "Acordei agorinha =P";
+        }else if(horas <= 12){
+            tempoOn = "Tá na hora de ir dormir já k"
+        }else if(dias <= 1){
+            tempoOn = "Alguém me deixa dormir pfv ;-;"
+        }else if(dias <= 7){
+            tempoOn = "Daqui a pouco já vai fazer uma semana, é sério, deixa eu dormir..."
+        }else{
+            tempoOn = "Virei sócia da RedBull, não preciso dormir mais \o/"
+        }
 
-const semDormir = new RichEmbed()
-.setAuthor(bot.user.username, bot.user.avatarURL)
-.addField(stripIndents`**> Dias:** ${dias}
-**> Horas:** ${horas}
-**> Minutos:** ${minutos}
-**> Segundos:** ${segundos}`, true)
-.setColor('#00ffff')
-.setFooter(tempoON)
-.setTitle("Quanto tempo eu estou sem dormir")
-.setThumbnail(bot.user.avatarURL);
+        const semDormir = new RichEmbed()
+        .setAuthor(bot.user.username, bot.user.avatarURL)
+        .addField(stripIndents`**> Dias:** ${dias}
+        **> Horas:** ${horas}
+        **> Minutos:** ${minutos}
+        **> Segundos:** ${segundos}`, true)
+        .setColor('#00ffff')
+        .setFooter(tempoON)
+        .setTitle("Quanto tempo eu estou sem dormir")
+        .setThumbnail(bot.user.avatarURL);
 
-message.channel.send(semDormir);  
+        message.channel.send(semDormir.toString());  
 
     }}
 
