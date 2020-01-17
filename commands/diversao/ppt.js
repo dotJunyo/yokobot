6d2f3e1c080c58d0308315b2e9d9e6fe725a1b13
Junyo <junyogabriel001@hotmail.com>
2020-01-16 21:46:52 -0300

    Create ppt.js

---
+++ commands/diversao/ppt.js
@@ -0,0 +1,47 @@
+const { RichEmbed } = require('discord.js');
+const { promptMessage } = require("../../functions.js");
+
+const chooseArr = ["🗻", "🧻", "✂"]
+
+const playAgain = ["🔁"]
+
+module.exports = {
+    name: "ppt",
+    aliases: ["rps"],
+    category: "diversao",
+    description: "Pedra, papel ou tesoura!!!",
+    example: "",
+    run: async(bot, message, args) => {
+        const pptEmbed = new RichEmbed()
+            .setColor("#ffffff")
+            .setFooter(message.guild.me.displayName, bot.user.displayAvatarURL)
+            .setDescription("Escolha um emoji para jogar!")
+            .setTimestamp();
+
+            const m = await message.channel.send(pptEmbed);
+            const reacted = await promptMessage(m, message.author, 30, chooseArr, playAgain);
+
+            const botChoice = chooseArr[Math.floor(Math.random() * chooseArr.length)];
+
+            const result = await getResult(reacted, botChoice);
+            await m.clearReactions();
+
+            pptEmbed
+                .setDescription("")
+                .addField(result, `${result} vs ${botChoice}`);
+
+            m.edit(pptEmbed);
+
+            function getResult(me, clientChosen){
+                if ((me === "🗻" && clientChosen === "✂") ||
+                    (me === "🧻" && clientChosen === "🗻") ||
+                    (me === "✂" && clientChosen === "🧻")){
+                        return 'Você ganhou! ||eu deixei...||'
+                    }else if (me === clientChosen){
+                        return "Empatamos! Assim todos ganham 😁"
+                    }else{
+                        return "Eu ganhei dessa vez xD"
+                    }
+            }
+
+    }}
