17552d4e556aec1218cd587cd71eb0ab4b01ebb9
Junyo <junyogabriel001@hotmail.com>
2020-01-24 05:14:01 -0300

    ModulesUpgrade

---
+++ commands/musica/play.js
@@ -0,0 +1,11 @@
+const { RichEmbed } = require('discord.js');
+
+module.exports = {
+    name: "",
+    aliases: [""],
+    category: "",
+    description: "",
+    example: "",
+    run: async(bot, message, args) => {
+
+    }}
