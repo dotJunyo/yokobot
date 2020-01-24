455713a0b877e720c11d749bbf164fe529783c69
Junyo <junyogabriel001@hotmail.com>
2020-01-24 06:17:35 -0300

    Update prefix.js

--- commands/moderation/prefix.js
+++ commands/moderation/prefix.js
@@ -6,13 +6,14 @@ module.exports = {
     run: async(bot, message, args) => {
         if(message.deletable) message.delete();
 
-        if(!args[1]){
+        let novoPrefix = message.content.replace('-prefix ', '')
+
+        if(!args[0]){
 
             message.reply("Você precisa definir outro prefix ;P")
 
         }else{
-
-            var prefix = args[1];
-            message.channel.send("Prefix mudado para: " + prefix);
+            prefix = novoPrefix;
+            message.channel.send("Prefix mudado para: " + novoPrefix);
 
     }}}
