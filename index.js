538ca91150849dcde4683ce947155d0fb362c3a1
Junyo <junyogabriel001@hotmail.com>
2020-01-31 03:41:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ bot.on ('message', message =>{
 
     //=================================SOWZX==============================
 
-    if(message.content('sowzx'/* || 'Sowzx' || '@sowzx' || '@Sowzx'*/)){
+    if(message.content.includes('sowzx'/* || 'Sowzx' || '@sowzx' || '@Sowzx'*/)){
         message.reply("Não fala do Sowzx, eca! Ele é muito estranho :c");
     }
 
