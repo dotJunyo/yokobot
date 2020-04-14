eeb01101f83884e6f6f521e6cf0ce83e607476d1
Junyo <44989305+JunyoTyber@users.noreply.github.com>
2020-04-13 21:45:31 -0300

    Update autor.js

--- commands/info/autor.js
+++ commands/info/autor.js
@@ -6,14 +6,12 @@ module.exports = {
     example: "-autor",
     run: async(bot, message, args) => {
 
-        var autorImg = "https://cdn.discordapp.com/avatars/295718290251317260/9055d701ffc0a6e440b12c790845b4d9.png?size=2048"
-
         if(message.deletable) message.delete();
 
         const autorEmbed = new Discord.RichEmbed()
         .setColor('#275BF0')
         .setTitle("Fui criada por Junyo#6937")
-        .setImage(autorImg)
+        .setImage("https://i.imgur.com/xgSyBDZ.png")
         message.channel.send(autorEmbed);
     },
 }
