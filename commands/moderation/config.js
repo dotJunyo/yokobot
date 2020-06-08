be0c8badfc982be128f0667bd793892074bd4cfc
Junyo <junyogabriel001@hotmail.com>
2020-06-07 22:33:18 -0300

    Update config.js

--- commands/moderation/config.js
+++ commands/moderation/config.js
@@ -3,3 +3,18 @@
 CANAL DE CONFIG DO BOT
 
 */
+
+const { RichEmbed } = require('discord.js');
+const { stripIndents} = require("common-tags");
+
+module.exports = {
+    name: "config",
+    category: "moderation",
+    description: "Minha configuração ^-^",
+    example: "-config",
+    run: async(bot, message, args) => {
+
+
+
+    }
+}
