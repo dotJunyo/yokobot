7a22e65f310ab78376bb1d74bede1bb764a8a7e4
Junyo <junyogabriel001@hotmail.com>
2020-06-09 23:34:39 -0300

    Update limpar.js

--- commands/moderation/limpar.js
+++ commands/moderation/limpar.js
@@ -9,9 +9,11 @@ module.exports = {
         let limparLinhas = message.content.replace('-limpar ', '')
 
         if (args[0]) {
-            message.channel.send(limparLinhas);
+            message.channel.send(limparLinhas).then(m => m.delete(5000));
             message.delete();
-            message.channel.bulkDelete(limparLinhas);
+            channel.bulkDelete(limparLinhas)
+                .then(messages => console.log(`Bulk deleted ${messages.size} messages`))
+                .catch(console.error);
             message.reply("Ufa! Limpar cansa um pouquinho 😅")
                 .then(m => m.delete(5000));
         } else {
